0da14572c0726aaa2d03d4cfdbabaf713ff5de22
Michittio <nguyennhutrung.nnt@gmail.com>
2023-10-08 20:42:28 +0700

    fix bug

--- api/index.js
+++ api/index.js
@@ -18,7 +18,15 @@ const Post = require("./models/Post");
 const app = express();
 
 // khi dùng credential phải thêm vài thông tin vào cors
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        allowedHeaders: "Content-Type,Authorization",
+        optionsSuccessStatus: 200, // Set the status code for successful preflight requests
+    })
+);
 // app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
