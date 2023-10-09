18d0d55cb93bb0d6c080594e18e60590077d6682
Michittio <nguyennhutrung.nnt@gmail.com>
2023-10-09 22:33:41 +0700

    update

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -48,7 +48,7 @@ export default function Header() {
     return (
         <header>
             <Link to="/" className="logo">
-                MyBlog
+                miBlog
             </Link>
             <nav>
                 {userInfo?.username ? (
